1b8fd2b91177ea85f03e85d15a5c282e8549a93f
RaiderNationBuilder <71417297+RaiderNationBuilder@users.noreply.github.com>
2021-09-06 18:46:37 -0500

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -75,9 +75,14 @@ var fight = function(enemyName) {
   };
 
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiator!  Round " + (i + 1) );
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50
+        fight(pickedEnemyName);
+    } else {
+        window.alert("You have lost your robot in battle! Game Over!");
+    }   
 }
 // run fight function to start game
 // fight();
